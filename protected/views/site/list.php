2663472294c75bab5a11babe04d218ea19962c75
Demon Zhang <demon.zhang@samesamechina.com>
2015-11-17 11:09:40 +0800

    no message

--- protected/views/site/list.php
+++ protected/views/site/list.php
@@ -1,5 +1,5 @@
 <link id="skin_style" href="/css/emoji.css" type="text/css" rel="stylesheet" charset="utf-8" /> 
-<center>
+<center style="font-size:12px">
 <?php
 for ($i = 0; $i < count($list); $i++) {
 	echo $list[$i]['nickname']."<br>";
